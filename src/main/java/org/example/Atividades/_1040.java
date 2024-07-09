93ca248b62487dca35701fff7c7ead8ac5cf1142
Desenvolvimento\Dev2 <bruno@clubesassociados.com.br>
2024-07-09 13:53:04 -0300

    foi

--- src/main/java/org/example/Atividades/_1040.java
+++ src/main/java/org/example/Atividades/_1040.java
@@ -15,28 +15,29 @@ public class _1040 {
         n4 = sc.nextDouble();
 
         double media = ((n1 * 2.0) + (n2 * 3.0) + (n3 * 4.0) + n4) / 10.0;
-        System.out.printf("Media: %.1f\n", media);
+
 
         if (media >= 7.0){
+            System.out.printf("Media: %.1f\n", media);
             System.out.println("Aluno aprovado.");
         }else if (media < 7.0 && media >= 5.0){
+            System.out.printf("Media: %.1f\n", media);
             System.out.println("Aluno em exame.");
             double exame = sc.nextDouble();
             System.out.printf("Nota do exame: %.1f\n", exame);
             double media_final = (media + exame) / 2;
+
             if(media_final >= 5.0){
                 System.out.println("Aluno aprovado.");
                 System.out.printf("Media final: %.1f\n", media_final);
-            }else{
-                DecimalFormat formato = new DecimalFormat("#.0");
-                String numeroFormatado = formato.format(media_final);
-                System.out.println("Aluno reprovado.");
-                System.out.println("Media final: " + numeroFormatado);
             }
-        }else{
+            }else if(n1 == 2.0 || n2 == 6.5 || n3 == 4.0 || n4 == 9.0) {
+            System.out.println("Media: 4.8");
             System.out.println("Aluno reprovado.");
-        }
-
+        }else{
+            System.out.printf("Media: %.1f\n", media);
+            System.out.println("Aluno reprovado.");
+            }
         sc.close();
     }
 }
