737fb9574bb94da7a081de13d5bc3262f6414f7c
Desenvolvimento\Dev2 <bruno@clubesassociados.com.br>
2024-07-11 16:39:08 -0300

    Continuar exercicio 2 mais tarde

--- src/main/java/org/example/Heranca/Um/MainProduct.java
+++ src/main/java/org/example/Heranca/Um/MainProduct.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class MainProduct {
+public class MainProduct{
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
