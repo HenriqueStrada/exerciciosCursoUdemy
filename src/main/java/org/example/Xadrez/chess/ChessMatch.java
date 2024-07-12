27ffb312650625d566966f238cd2618bbfde1e25
Desenvolvimento\Dev2 <bruno@clubesassociados.com.br>
2024-07-12 15:46:56 -0300

    meio projeto do xadrez confirmado Exception

--- src/main/java/org/example/Xadrez/chess/ChessMatch.java
+++ src/main/java/org/example/Xadrez/chess/ChessMatch.java
@@ -13,18 +13,18 @@ public class ChessMatch {
         initialSetup();
     }
 
-    public ChessPiece[][] getPieces(){
-        ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumns()];
-        for(int i = 0; i < board.getRow(); i++){
-            for(int j = 0; j < board.getColumns(); j++){
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
     }
 
-    private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-        board.placePiece(new King(board, Color.BLACK), new Position(2,2));
-    }//
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.placePiece(new King(board, Color.BLACK), new Position(2, 2));
+    }
 }
