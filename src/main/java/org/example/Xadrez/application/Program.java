121a7745023b3a0d0ecd600faad0f003bf83a276
Desenvolvimento\Dev2 <bruno@clubesassociados.com.br>
2024-07-12 16:43:30 -0300

    Meio projeto xadrez, exeções de movimentação das peças

--- src/main/java/org/example/Xadrez/application/Program.java
+++ src/main/java/org/example/Xadrez/application/Program.java
@@ -2,10 +2,12 @@ package org.example.Xadrez.application;
 
 import org.example.Xadrez.boardgame.Board;
 import org.example.Xadrez.boardgame.Position;
+import org.example.Xadrez.chess.ChessException;
 import org.example.Xadrez.chess.ChessMatch;
 import org.example.Xadrez.chess.ChessPiece;
 import org.example.Xadrez.chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -14,14 +16,25 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while(true){
-            UI.printBoard(chessMatch.getPieces());
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }catch(ChessException e){
+                System.out.println(e.getMessage());
+                System.out.print("Press enter to continue");
+                sc.nextLine();
+            }catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                System.out.print("Press enter to continue");
+                sc.nextLine();
+            }
         }
     }
 }
