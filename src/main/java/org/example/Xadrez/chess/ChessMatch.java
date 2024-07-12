7c7387ebe7d629f700d04581968138ffd588f233
Desenvolvimento\Dev2 <bruno@clubesassociados.com.br>
2024-07-12 17:05:51 -0300

    Meio projeto xadrez, exeções de movimentação das peças

--- src/main/java/org/example/Xadrez/chess/ChessMatch.java
+++ src/main/java/org/example/Xadrez/chess/ChessMatch.java
@@ -43,6 +43,9 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
         }
+        if(board.piece(position)){
+
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
--- src/main/java/org/example/Xadrez/chess/ChessPiece.java
+++ src/main/java/org/example/Xadrez/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package org.example.Xadrez.chess;
 import org.example.Xadrez.boardgame.Board;
 import org.example.Xadrez.boardgame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
     private Color color;
 
     public ChessPiece(Board board, Color color) {
