a1690b731b8dc03510baf105db8473186b1bf203
Desenvolvimento\Dev2 <bruno@clubesassociados.com.br>
2024-07-15 13:09:32 -0300

    Logica da rainha

--- src/main/java/org/example/Xadrez/chess/ChessMatch.java
+++ src/main/java/org/example/Xadrez/chess/ChessMatch.java
@@ -200,6 +200,7 @@ public class ChessMatch {
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
@@ -216,5 +217,6 @@ public class ChessMatch {
         placeNewPiece('g', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK ));
         placeNewPiece('f', 8, new Knight(board, Color.BLACK ));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
     }
 }
